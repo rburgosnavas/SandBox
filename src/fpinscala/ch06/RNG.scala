dbc50edb82c2d9909027a358f76493d87198cb4d
burgosr <roby0b@gmail.com>
2015-04-08 13:58:03 -0700

    chapter 6 work

--- src/fpinscala/ch06/RNG.scala
+++ src/fpinscala/ch06/RNG.scala
@@ -23,3 +23,49 @@ case class SimpleRNG(seed: Long) extends RNG {
     (n, nextRNG)
   }
 }
+
+object RNG {
+  // Ex. 6.1
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val i = rng.nextInt
+    if (i._1 >= 0 && i._1 <= Int.MaxValue) {
+      i
+    } else {
+      nonNegativeInt(i._2)
+    }
+  }
+
+  // Ex 6.2
+  def double(rng: RNG): (Double, RNG) = {
+    val (d, r) = nonNegativeInt(rng)
+    ((d / (Int.MaxValue + 1)).toDouble, r)
+  }
+
+  // Ex 6.3
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r) = rng.nextInt
+    val (d, r2) = double(r)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
+
+  // Ex 6.4
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def loop(cnt: Int, curr: RNG, xs: List[Int]): (List[Int], RNG) = {
+      val (x, r) = curr.nextInt
+      if (cnt > 0) loop(cnt - 1, r, x::xs) else (xs, r)
+    }
+    loop(count, rng, Nil)
+  }
+}
--- src/fpinscala/ch06/RNGRunner.scala
+++ src/fpinscala/ch06/RNGRunner.scala
@@ -10,4 +10,21 @@ object RNGRunner extends App {
   println(n1._2.nextInt)
   println(n1._2.nextInt._2.nextInt)
   println(n1._2.nextInt._2.nextInt._2.nextInt)
+
+  // Ex. 6.1
+  println(RNG.nonNegativeInt(r1))
+  println(RNG.nonNegativeInt(n1._2.nextInt._2))
+
+  // Ex. 6.1
+  println(RNG.double(n1._2))
+  println(RNG.double(n1._2.nextInt._2))
+  println(RNG.double(n1._2.nextInt._2.nextInt._2))
+
+  // Ex 6.3
+  println(RNG.intDouble(n1._2.nextInt._2.nextInt._2))
+  println(RNG.doubleInt(n1._2.nextInt._2.nextInt._2))
+  println(RNG.double3(n1._2.nextInt._2.nextInt._2))
+
+  // Ex 6.4
+  println(RNG.ints(5)(r1))
 }
